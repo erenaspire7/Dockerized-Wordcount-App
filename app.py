e0155c03919c36d77b7270a30d46e6df10010fc4
Yusuff Jamal <yusuffjamal3@gmail.com>
2020-06-07 04:21:35 +0200

    Upgrade

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
 
 @app.route('/')
 def hello():
